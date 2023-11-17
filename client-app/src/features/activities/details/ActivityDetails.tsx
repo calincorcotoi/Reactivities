b43ba30162c4265fa08ca8ba6bd928500a9e3c6b
Calin Corcotoi <calin.corcotoi@veoneer.com>
2023-11-17 08:39:45 +0200

    fnish crud operation

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,43 @@
+import { Activity } from "../../../app/models/activity";
+import { Button, Card, Image } from "semantic-ui-react";
+
+interface Prop {
+  activity: Activity;
+  cancelSelectActivity: () => void;
+  openForm: (id?: string) => void;
+}
+
+export default function ActivityDetails({
+  activity,
+  cancelSelectActivity,
+  openForm,
+}: Prop) {
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths="2">
+          <Button
+            onClick={() => openForm(activity.id)}
+            basic
+            color="blue"
+            content="Edit"
+          />
+          <Button
+            onClick={cancelSelectActivity}
+            basic
+            color="grey"
+            content="Cancel"
+          />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+}
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,94 @@
+import { ChangeEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+  closeForm: () => void;
+  editOrCreate: (activity: Activity) => void;
+  activity: Activity | undefined;
+}
+
+export default function ActivityForm({
+  activity: selectedActivity,
+  closeForm,
+  editOrCreate,
+}: Props) {
+  const initialState = selectedActivity ?? {
+    id: "",
+    title: "",
+    date: "",
+    description: "",
+    category: "",
+    city: "",
+    venue: "",
+  };
+
+  const [activity, setActivity] = useState(initialState);
+
+  function handleChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value });
+  }
+
+  function handleSubmit() {
+    editOrCreate(activity);
+  }
+
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input
+          placeholder="Title"
+          name="title"
+          value={activity?.title}
+          onChange={handleChange}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          name="description"
+          value={activity?.description}
+          onChange={handleChange}
+        />
+        <Form.Input
+          placeholder="Category"
+          name="category"
+          value={activity?.category}
+          onChange={handleChange}
+        />
+        <Form.Input
+          placeholder="Date"
+          name="date"
+          value={activity?.date}
+          onChange={handleChange}
+        />
+        <Form.Input
+          placeholder="City"
+          name="city"
+          value={activity?.city}
+          onChange={handleChange}
+        />
+        <Form.Input
+          placeholder="Venue"
+          name="venue"
+          value={activity?.venue}
+          onChange={handleChange}
+        />
+      </Form>
+      <Button
+        onClick={handleSubmit}
+        floated="right"
+        positive
+        type="submit"
+        content="Submit"
+      />
+      <Button
+        onClick={closeForm}
+        floated="right"
+        type="button"
+        content="Cancel"
+      />
+    </Segment>
+  );
+}
